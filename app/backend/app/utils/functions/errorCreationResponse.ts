176dfbe4f14f702ab240bba4068d4d773e5ae916
Timeuh <timeuuh@gmail.com>
2025-08-09 15:46:31 +0200

    📝 fix doc for this function

--- app/backend/app/utils/functions/errorCreationResponse.ts
+++ app/backend/app/utils/functions/errorCreationResponse.ts
@@ -1,10 +1,9 @@
 import { ApiErrorResponse } from "#utils/types/api";
 
 /**
- * Response returned when successfully creating a new entry in database
- * 
- * @param data : the new entry
- * @param resourceName : the name of the resource type
+ * Response returned when error in creating a new entry in database
+ *
+ * @param message : the error message
  */
 export const errorCreationResponse = (message: string): ApiErrorResponse => {
   return {
